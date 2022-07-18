33dee170d55d9ce3f8975f95edf99eafe8822317
SpecialAPI <pavlov.andrei.d@gmail.com>
2022-07-18 22:54:47 +0300

    what is a patcher

---
+++ MTGAPIPatcher/Object.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MonoMod;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+namespace MTGAPIPatcher
+{
+    [MonoModPatch("UnityEngine.Object")]
+    internal class patch_Object : Object
+    {
+        public static new T Instantiate<T>(T original) where T : Object
+        {
+            return (T)(object)orig_Instantiate(original);
+        }
+
+        public static new Object Instantiate(Object original)
+        {
+            return orig_Instantiate(original);
+        }
+
+        public static extern Object orig_Instantiate(Object original);
+
+
+    }
+}
