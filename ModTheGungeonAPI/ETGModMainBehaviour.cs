94a75636fe6eb584ab8d85e6adb0e788b2e711ee
SpecialAPI <pavlov.andrei.d@gmail.com>
2023-12-21 13:12:27 +0300

    update version number

--- ModTheGungeonAPI/ETGModMainBehaviour.cs
+++ ModTheGungeonAPI/ETGModMainBehaviour.cs
@@ -24,7 +24,7 @@ public class ETGModMainBehaviour : BaseUnityPlugin
     /// <summary>
     /// The current version of the MTG API.
     /// </summary>
-    public const string VERSION = "1.7.1";
+    public const string VERSION = "1.7.2";
     /// <summary>
     /// Current instance of the MTG API behaviour.
     /// </summary>
